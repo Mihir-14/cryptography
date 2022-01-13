f8caa05e235ee433975545441074cdf11e2e24a2
Mihir-14 <97664801+Mihir-14@users.noreply.github.com>
2022-01-13 14:27:21 +0530

    Add files via upload

---
+++ caeser_cipher_decryptor.py
@@ -0,0 +1,17 @@
+
+
+print("Welcome!")
+print("Enter the key for decryption: ")
+k=int(input())
+print("\nEnter Cipher text: ")
+cipher=(input())
+mesg=''
+for i in range(len(cipher)):
+	y=cipher[i]
+	z=y.lower()
+	if z==" ":
+		mesg=mesg+' '
+	elif y.isalpha():
+		mesg=mesg+chr((ord(z)-k-97)%26+97)		#97 is unicode for letter 'a'
+print("\nYour decrytped Message is: ")
+print (mesg)
